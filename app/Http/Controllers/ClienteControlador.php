d1bd3108c6d6429436c93a3a9cf0178c390b68d9
RogerioPires011 <44650582+RogerioPires011@users.noreply.github.com>
2019-02-21 13:03:37 -0400

    initial commit

--- app/Http/Controllers/ClienteControlador.php
+++ app/Http/Controllers/ClienteControlador.php
@@ -6,79 +6,58 @@ use Illuminate\Http\Request;
 
 class ClienteControlador extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    
     public function index()
     {
-        //
+        return "Listas de todos os Cliente- Raiz";
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
-        //
+        return "Formulario para cadastrar novo Cliente";
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
-        //
+        $s="Armazenar:";
+        $s .="Nome: ".$request->input('nome') ." e ";
+        $s .="Idade: ".$request->input('idade');
+        return response ($s,201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $v=["Mario","Edson","Roberto","Jean"];
+        if($id >=0 && $id <count($v))
+            return$v[$id];
+        return"Nao Encontrado";
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        return"Formulario para editar Clientes ID " .$id;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
-        //
+        $s="Atualizar cliente com id $id: ";
+        $s .="Nome: ".$request->input('nome') ." e ";
+        $s .="Idade: ".$request->input('idade');
+        return response ($s,200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function destroy($id)
     {
-        //
+        return response("Apagando clinte com id $id",200); 
     }
+
+
+     public function requisitar(Request $request){
+        echo"nome: " . $request->input('nome'); 
+     }
 }
